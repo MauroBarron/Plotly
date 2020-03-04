d095085cff22ed0838d20126307c62376c6bc4ad
unknown <josemaurobarron@gmail.com>
2020-03-03 17:01:24 -0800

    Challange Save Point

--- plots.js
+++ plots.js
@@ -30,12 +30,27 @@ function buildMetadata(sample) {
     var result = resultArray[0];
     var PANEL = d3.select("#sample-metadata");
     PANEL.html("");
-    PANEL.append("h6").text(result.id);
-    PANEL.append("h6").text(result.ethnicity);
-    PANEL.append("h6").text(result.gender);
-    PANEL.append("h6").text(result.age);
-    PANEL.append("h6").text(result.location);
-    PANEL.append("h6").text(result.bbtype);
-    PANEL.append("h6").text(result.wfreq);
+
+    // console.log(Object.entries(result)[0]);
+    
+    Object.entries(result).forEach(([demographic, dmgValue]) => {
+      PANEL.append("h6").text(demographic.toUpperCase() + " : " + dmgValue);
+    });
   });
 }
+  //  Object.entries(filters).forEach(([key, value]) => {
+  //     filteredData = filteredData.filter(row => row[key] === value); });
+
+    // {'a':1}
+    // ['a', 1]
+
+    // dmgresult.forEach(([key, value]) => console.log(first
+    //   + ": " + second));
+
+    // PANEL.append("h6").text(result.id);
+    // PANEL.append("h6").text(result.ethnicity);
+    // PANEL.append("h6").text(result.gender);
+    // PANEL.append("h6").text(result.age);
+    // PANEL.append("h6").text(result.location);
+    // PANEL.append("h6").text(result.bbtype);
+    // PANEL.append("h6").text(result.wfreq);
